7d7ad91df0f9b110065e9ba04ccf4d54ec9232f6
namwookkim <havenlys@nate.com>
2012-11-20 16:34:06 +0900

    test project

--- src/project/MyServlet.java
+++ src/project/MyServlet.java
@@ -29,7 +29,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("../index.jsp");
 		rd.forward(request, response);		
 	}
 
