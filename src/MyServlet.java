7389799ada196767cd7d98dbdf6c51994be07e27
namwookkim <havenlys@nate.com>
2012-12-01 16:00:08 +0900

    Servlet Test

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -27,6 +27,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 	}
 
 	/**
